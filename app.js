ededa798d23bf67ce575996cfe2ea9ca6861dd78
cepiseptiyana <cepifams3@gmail.com>
2024-06-02 19:44:08 +0700

    upload

---
+++ app.js
@@ -0,0 +1,33 @@
+let login = document.getElementsByClassName("login")[0];
+login.addEventListener("click", async function () {
+  // property
+  const username = document.getElementById("username").value;
+  const password = document.getElementById("password").value;
+  const resultDatabase = await database();
+
+  // find
+  function findContact(nama, password) {
+    const username = resultDatabase.find((data) => {
+      return (
+        data.username.toLowerCase() === nama.toLowerCase() &&
+        data.password.toLowerCase() === password.toLowerCase()
+      );
+    });
+    return username;
+  }
+
+  if (findContact(username, password)) {
+    window.location.href = "home.html";
+    console.log("ok");
+  } else {
+    window.location.href = "app.html";
+    alert(`data gada`);
+    console.log("data gada");
+  }
+});
+
+async function database() {
+  return fetch("database.json")
+    .then((result) => result.json())
+    .then((data) => data);
+}
