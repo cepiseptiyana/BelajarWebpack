2137026cac3c1ad197f13f034836b253e49b82fd
cepiseptiyana <cepifams3@gmail.com>
2024-06-02 19:53:03 +0700

    q

--- app.js
+++ app.js
@@ -1,33 +1,33 @@
-let login = document.getElementsByClassName("login")[0];
-login.addEventListener("click", async function () {
-  // property
-  const username = document.getElementById("username").value;
-  const password = document.getElementById("password").value;
-  const resultDatabase = await database();
+// let login = document.getElementsByClassName("login")[0];
+// login.addEventListener("click", async function () {
+//   // property
+//   const username = document.getElementById("username").value;
+//   const password = document.getElementById("password").value;
+//   const resultDatabase = await database();
 
-  // find
-  function findContact(nama, password) {
-    const username = resultDatabase.find((data) => {
-      return (
-        data.username.toLowerCase() === nama.toLowerCase() &&
-        data.password.toLowerCase() === password.toLowerCase()
-      );
-    });
-    return username;
-  }
+//   // find
+//   function findContact(nama, password) {
+//     const username = resultDatabase.find((data) => {
+//       return (
+//         data.username.toLowerCase() === nama.toLowerCase() &&
+//         data.password.toLowerCase() === password.toLowerCase()
+//       );
+//     });
+//     return username;
+//   }
 
-  async function database() {
-    return fetch("database.json")
-      .then((result) => result.json())
-      .then((data) => data);
-  }
+//   async function database() {
+//     return fetch("database.json")
+//       .then((result) => result.json())
+//       .then((data) => data);
+//   }
 
-  if (findContact(username, password)) {
-    window.location.href = "home.html";
-    console.log("ok");
-  } else {
-    window.location.href = "app.html";
-    alert(`data gada`);
-    console.log("data gada");
-  }
-});
+//   if (findContact(username, password)) {
+//     window.location.href = "home.html";
+//     console.log("ok");
+//   } else {
+//     window.location.href = "app.html";
+//     alert(`data gada`);
+//     console.log("data gada");
+//   }
+// });
